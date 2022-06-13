95ceaa7018ee7498373af445574a8d8d6bdc2361
Piza01134 <99108487+Piza01134@users.noreply.github.com>
2022-06-13 14:42:37 -0700

    added histogram and npp

--- DataGrapher.py
+++ DataGrapher.py
@@ -23,17 +23,37 @@ def find_rain_data(year: str, month: int, day: int):
                     return row[23].value
 
 
+#create histogram
+def hist(x, y):
+    if not len(x) == len(y):
+        return None
+    
+    plt.hist(y)
+    show_graph("Amount", "Frequency", "Histogram")   
+
 #create normal probablity plot
 def npp(x, y):
     if not len(x) == len(y):
         return None
-        
-    show_graph(x, y, "Dates", "Precipitation in mm", "Precipitation vs. Time")
+    
+    x = sorted(x)
+    y = sorted(y)
+
+    num = len(x)
+
+    temp = []
+    z_scores = []
+
+    for i in range(num):
+        temp.append( (i - 0.375) / (y[i] + 0.25) )
+
+    z_scores = stats.zscore(temp)
+
+    plt.scatter(x, z_scores)
+    show_graph("Dates", "Expected z-score", "Normal Probability Plot")
 
 #display graph
-def show_graph(x, y, x_label, y_label, title):
-    # plotting the points 
-    plt.scatter(x, y)
+def show_graph(x_label, y_label, title):
 
     # naming the x axis
     plt.xlabel(x_label)
@@ -46,6 +66,15 @@ def show_graph(x, y, x_label, y_label, title):
     # function to show the plot
     plt.show()
 
+#switch between options
+def graph_swtich(type, x, y):
+    match type:
+        case 0:
+            return npp(x, y)
+        case 1:
+            return hist(x, y)
+
+
 #load spreadsheet
 wb = load_workbook('Rain Spreadsheet.xlsx')
 
@@ -54,30 +83,35 @@ text = file.read()
 date = []
 dates = text.split("\n")
 
-rain = []
+rain_amount = []
 for date in dates:
-    rain.append( find_rain_data( date[0:4], int(date[5:7]), int(date[8:10]) ) )
+    rain_amount.append( find_rain_data( date[0:4], int(date[5:7]), int(date[8:10]) ) )
 
 # x axis values
+#x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 x = dates
+
 # corresponding y axis values
-y = rain
+#y = [5, 5, 5, 5, 2000, 5, 5, 5, 26, 313, 20]
+y = rain_amount
 
-z_scores = stats.zscore(y)
+type = 1
+while not type == -1:
+
+    type = int( input(""
+                + "-1 for cancel" + "\n"
+                + "0 for Normal Probability Plot" + "\n"
+                + "1 for Histogram" 
+                + ": ") )
 
 
-# plotting the points 
-plt.scatter(x, y)
-  
-# naming the x axis
-plt.xlabel('Dates')
-# naming the y axis
-plt.ylabel('Precipitation in mm')
-  
-# giving a title to my graph
-plt.title('Precipitation vs. Time')
-  
-# function to show the plot
-plt.show()
+    while not type in range(-1, 2):
+        type = int( input(""
+                    + "-1 for cancel" + "\n"
+                    + "0 for Normal Probability Plot" + "\n"
+                    + "1 for Histogram" 
+                    + ": ") )
+
+    graph_swtich(type, x, y)
 
 file.close
